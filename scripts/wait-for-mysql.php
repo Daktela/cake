30e4ec74450dd338a761735066ea4b416a0055ae
JanTcz <104137790+JanTcz@users.noreply.github.com>
2022-05-30 20:36:05 +0200

    Update wait-for-mysql.php

--- scripts/wait-for-mysql.php
+++ scripts/wait-for-mysql.php
@@ -2,7 +2,17 @@
 
 $dsn = [];
 
-require "/tmp/databases.php";
+require("vendor/autoload.php");
+
+$dotenv = new \josegonzalez\Dotenv\Loader(['.env']);
+$dotenv = $dotenv->parse()->toArray();
+
+$dsn[$dotenv['MYSQL_DATABASE']] = [
+        'dsn' => 'mysql:host='.$dotenv["MYSQL_HOST"].':3306;dbname='.$dotenv["MYSQL_DATABASE"],
+        'database' => $dotenv['MYSQL_DATABASE'],
+        'user' => $dotenv['MYSQL_USER'],
+        'pass' => $dotenv['MYSQL_PASSWORD']
+    ];
 
 $connections = [];
 $checked = [];
