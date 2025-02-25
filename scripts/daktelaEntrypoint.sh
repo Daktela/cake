23b79b82867ebe3fc3bb510fc78e96c4ab494419
Dobroslav Tokoš <dobroslav@tokos.sk>
2025-02-25 11:14:11 +0100

    Fixed composer paths for PHP 8.2

--- scripts/daktelaEntrypoint.sh
+++ scripts/daktelaEntrypoint.sh
@@ -7,9 +7,9 @@ NC='\033[0m' # No Color
 mkdir -p ./tmp
 mkdir -p ./logs
 
-php82 composer install --prefer-dist --no-interaction --classmap-authoritative
-php82 composer dump-autoload --optimize
-php82 composer clear-cache --quiet
+php82 /usr/local/bin/composer install --prefer-dist --no-interaction --classmap-authoritative
+php82 /usr/local/bin/composer dump-autoload --optimize
+php82 /usr/local/bin/composer clear-cache --quiet
 
 # Disable XDEBUG when installing
 XDEBUG_MODE_OLD=$XDEBUG_MODE
--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -10,7 +10,7 @@ OWNER=www-data:www-data
 case $1 in
 
   create)
-      php82 composer create-project --no-interaction --prefer-dist cakephp/app:"$2" .
+      php82 /usr/local/bin/composer create-project --no-interaction --prefer-dist cakephp/app:"$2" .
     ;;
 
   create-and-up)
@@ -18,7 +18,7 @@ case $1 in
       if [ -z "$(ls -A .)" ]; then
       	printf "${BLUE}Creating project.${NC}\n"
 
-      	php82 composer create-project --no-interaction --prefer-dist cakephp/app:"$2" .
+      	php82 /usr/local/bin/composer create-project --no-interaction --prefer-dist cakephp/app:"$2" .
 
         cp /tmp/app_local.example.php config/app_local.php
 
